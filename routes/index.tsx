fbb0727617629279403fa9bd4bbc936272b3b3ec
locothedev <elias@moralis.io>
2022-08-09 19:33:03 +0200

    feat: coming soon

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,8 +5,11 @@ export default function Home() {
   return (
     <div>
       <h1>What does USDC do?</h1>{" "}
-      <h3>Answer: Playing a centralized roles in a decentralized world</h3>
+      <h3>Answer: Playing a centralized role in a decentralized world</h3>
       <a href="/blacklist">See latest Blacklisting Events</a>
+      <span>
+        More coming soon... and yeah... it will look better. I promise.
+      </span>
     </div>
   );
 }
