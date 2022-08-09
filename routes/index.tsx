6a4b21403ec016fc30cab77faeeef9b6897b164f
locothedev <elias@moralis.io>
2022-08-09 19:33:39 +0200

    fix: break line

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div>
       <h1>What does USDC do?</h1>{" "}
       <h3>Answer: Playing a centralized role in a decentralized world</h3>
-      <a href="/blacklist">See latest Blacklisting Events</a>
+      <a href="/blacklist">See latest Blacklisting Events</a> <br />
       <span>
         More coming soon... and yeah... it will look better. I promise.
       </span>
